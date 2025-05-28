8ab8290e6a3b0216e1286e16278dc45c6a499fc5
Susep S <susep.nutech@gmail.com>
2025-05-28 11:14:31 +0700

    update

--- src/modules/users/controllers/user.controller.js
+++ src/modules/users/controllers/user.controller.js
@@ -3,15 +3,11 @@ const router = express.Router();
 const model = require('../models/user.model');
 
 router.get('/', async (req, res) => {
-  try {
-    const users = await model.getUsers();
-    if (result.length == 0) {
-      return res.json({ message: 'Data not found!' });
-    }
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ message: 'Failed to fetch users' });
+  let result = await model.getUsers();
+  if (result.length == 0) {
+    return res.json({ message: 'Data not found!' });
   }
+  res.json(users);
 });
 
 module.exports = router;
