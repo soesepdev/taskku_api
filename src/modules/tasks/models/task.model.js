790217bc199a9d68ead40f75d29b278d04576f5f
Susep S <susep.nutech@gmail.com>
2025-06-03 16:14:24 +0700

    update

--- src/modules/tasks/models/task.model.js
+++ src/modules/tasks/models/task.model.js
@@ -2,7 +2,7 @@ const db = require('../../../config/db');
 
 const getTasks = async (id = '') => {
     try {
-        return await db.any('SELECT * FROM tasks');
+        return await db.any('SELECT t.id, t.title, t.description FROM tasks t');
     } catch (err) {
         console.log(err);
         return false;
