0f3c6e22b77f91be2259416575c0ccd9a5259374
Susep S <susep.nutech@gmail.com>
2025-06-03 15:01:37 +0700

    update : query status

--- src/modules/status/models/status.model.js
+++ src/modules/status/models/status.model.js
@@ -2,7 +2,7 @@ const db = require('../../../config/db');
         
 const getStatus = async () => {
     try {
-        return await db.any('SELECT * FROM status');
+        return await db.any('SELECT a.name, b.icon FROM status a LEFT JOIN icons b ON a.icon_id=b.id');
     } catch (err) {
         console.log(err);
         return false;
