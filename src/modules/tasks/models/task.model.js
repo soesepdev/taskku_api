2523cf762f530eba657bb88fd32867da01281e3a
Susep S <susep.nutech@gmail.com>
2025-06-03 16:29:30 +0700

    update

--- src/modules/tasks/models/task.model.js
+++ src/modules/tasks/models/task.model.js
@@ -2,7 +2,24 @@ const db = require('../../../config/db');
 
 const getTasks = async (id = '') => {
     try {
-        return await db.any('SELECT t.id, t.title, t.description FROM tasks t');
+        return await db.any(`SELECT 
+            t.id, 
+            t.title, 
+            t.description, 
+            u.full_name AS user_name, 
+            p.project_name, 
+            s.status_name, 
+            i.icon,
+            t.created_at,
+            t.updated_at,
+            t.is_active,
+            t.is_deleted
+            FROM tasks t 
+            LEFT JOIN users u ON t.user_id=u.id 
+            LEFT JOIN projects p ON t.project_id=p.id 
+            LEFT JOIN status s ON t.status_id=s.id 
+            LEFT JOIN icons i ON s.icon_id=i.id
+        `);
     } catch (err) {
         console.log(err);
         return false;
