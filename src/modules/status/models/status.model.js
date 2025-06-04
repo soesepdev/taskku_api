acf9a832e52db0e1aeb609de81df6fb989d8bb81
Susep S <susep.nutech@gmail.com>
2025-06-04 14:08:18 +0700

    update

--- src/modules/status/models/status.model.js
+++ src/modules/status/models/status.model.js
@@ -2,7 +2,17 @@ const db = require('../../../config/db');
         
 const getStatus = async () => {
     try {
-        return await db.any('SELECT s.id, s.status_name, i.icon, s.is_active, s.is_deleted FROM status s LEFT JOIN icons i ON s.icon_id=i.id');
+        return await db.any(`
+            SELECT 
+                s.id, 
+                s.status_name, 
+                i.icon, 
+                s.is_active, 
+                s.is_deleted
+            FROM status s 
+            LEFT JOIN icons i ON s.icon_id=i.id
+            ORDER BY s.seq ASC
+        `);
     } catch (err) {
         console.log(err);
         return false;
